6cd23de2e90eeb50165bc11918f9a8a37ef07e5d
Adrienn Tulak-Toth <tulaktothadrienn@gmail.com>
2022-06-27 14:14:18 +0200

    del try-except

--- test/test_login.py
+++ test/test_login.py
@@ -1,13 +1,13 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-from testdatas import test_user, registration_msg, registration_error
+
 import base_functions
 from testdatas import *
-from selenium.webdriver.chrome.options import Options
 
 
 class TestConduitLogin(object):
@@ -27,21 +27,17 @@ class TestConduitLogin(object):
 	def test_register(self):
 		# Vizsgáljuk meg a regisztrációt negatív ágon, már létező adatokkal.
 		# Ehhez először elvégezzük a regisztrációt, majd kilépünk
-		try:
-			base_functions.registration(self.browser, test_user['username'], test_user['user_email'], test_user['user_pwd'])
-			WebDriverWait(self.browser, 20).until(
-				EC.visibility_of_element_located((By.XPATH, '//button[@class="swal-button swal-button--confirm"]'))).click()
-			time.sleep(0.5)
-			WebDriverWait(self.browser, 20).until(
-				EC.visibility_of_element_located((By.XPATH, '//a[contains(text(), "Log out")]'))).click()
-			# Újraregisztrálunk a már felhasznált adatokkal
-			base_functions.registration(self.browser, test_user['username'], test_user['user_email'], test_user['user_pwd'])
-			WebDriverWait(self.browser, 30).until(
-				EC.visibility_of_element_located((By.XPATH, '//div[@class="swal-icon swal-icon--error"]')))
-		except:
-			assert False
-		else:
-			assert True
+		base_functions.registration(self.browser, test_user['username'], test_user['user_email'], test_user['user_pwd'])
+		WebDriverWait(self.browser, 20).until(
+			EC.visibility_of_element_located((By.XPATH, '//button[@class="swal-button swal-button--confirm"]'))).click()
+		time.sleep(0.5)
+		WebDriverWait(self.browser, 20).until(
+			EC.visibility_of_element_located((By.XPATH, '//a[contains(text(), "Log out")]'))).click()
+		# Újraregisztrálunk a már felhasznált adatokkal
+		base_functions.registration(self.browser, test_user['username'], test_user['user_email'], test_user['user_pwd'])
+		WebDriverWait(self.browser, 30).until(
+			EC.visibility_of_element_located((By.XPATH, '//div[@class="swal-icon swal-icon--error"]')))
+
 		# Ellenőrizzük, hogy a megfelelő hibaüzenet megjelenik
 		registration_error_title = WebDriverWait(self.browser, 30).until(
 			EC.visibility_of_element_located((By.XPATH, '//div[@class="swal-title"]')))
@@ -55,12 +51,7 @@ class TestConduitLogin(object):
 		error_msg_button.click()
 
 	def test_sign_in(self):  # Ellenőrizzük a bejelentkezést pozitív ágon
-		try:
-			base_functions.login(self.browser, test_user['user_email'], test_user['user_pwd'])
-		except:
-			assert False
-		else:
-			assert True
+		base_functions.login(self.browser, test_user['user_email'], test_user['user_pwd'])
 		# Ellenőrizzük, hogy bejelentkezést követően a főoldalra kerültünk.
 		assert self.browser.current_url == main_page
 		# Ellenőrizzük, hogy a regisztrált felhasználónév megjelenik a menüpontok között
@@ -69,17 +60,13 @@ class TestConduitLogin(object):
 		assert profile[2].text == test_user['username']
 
 	def test_cookie_bar(self):  # Vizsgáljuk meg az adatkezelési nyilatkozat működését
-		try:
-			cookie_panel = self.browser.find_element_by_xpath('//div[@id="cookie-policy-panel"]')
-			assert cookie_panel.is_displayed()
+		cookie_panel = self.browser.find_element_by_xpath('//div[@id="cookie-policy-panel"]')
+		assert cookie_panel.is_displayed()
 
-			cookie_accept_btn = self.browser.find_element_by_xpath(
-				'//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')
-			cookie_accept_btn.click()
-			time.sleep(0.5)
-		except:
-			assert False
-		else:
-			assert True
-		# lista-ként rákeresünk az eltűnő panelre és nullának vesszük a hosszát, azaz nem jelenik meg az oldalon
+		cookie_accept_btn = self.browser.find_element_by_xpath(
+			'//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')
+		cookie_accept_btn.click()
+		time.sleep(0.5)
+
+		# lista-ként rákeresünk az eltűnő panelre és nullának vesszük a hosszát, azaz ellenőrizzük, hogy nem jelenik meg az oldalon
 		assert len(self.browser.find_elements_by_xpath('//div[@id="cookie-policy-panel"]')) == 0
--- test/test_main.py
+++ test/test_main.py
@@ -8,7 +8,7 @@ from base_functions import *
 from testdatas import *
 
 
-class TestConduit(object):
+class TestConduitMain(object):
 
 	def setup(self):
 		browser_options = Options()
@@ -24,15 +24,11 @@ class TestConduit(object):
 		self.browser.quit()
 
 	def test_logout(self):  # Vizsgáljuk meg a kijelentkezés funkciót
-		try:
-			logout_nav = self.browser.find_element_by_xpath('//a[contains(text(), "Log out")]')
-			logout_nav.click()
+		logout_nav = self.browser.find_element_by_xpath('//a[contains(text(), "Log out")]')
+		logout_nav.click()
 
-			WebDriverWait(self.browser, 20).until(EC.presence_of_element_located((By.XPATH, '//a[@href="#/login"]')))
-		except:
-			assert False
-		else:
-			assert True
+		WebDriverWait(self.browser, 20).until(EC.presence_of_element_located((By.XPATH, '//a[@href="#/login"]')))
+
 		# Kijelentkezés után ellenőrizzük, hogy a Sign in és Sign up menüpontok jelennek csak meg
 		assert len(self.browser.find_elements_by_xpath('//a[@class="nav-link"]')) == 2
 		assert self.browser.find_element_by_xpath('//a[@href="#/login" and @class="nav-link"]').is_enabled()
@@ -41,19 +37,16 @@ class TestConduit(object):
 	def test_datas_to_list(self):
 		# Adatok listázása
 		# Szűrjünk rá és listázzuk ki a 'loret' tag-et tartalmazó article-ket
-		try:
-			loret_tag = self.browser.find_element_by_xpath('//div[@class="sidebar"]/div/a[@href="#/tag/loret"]')
-			loret_tag.click()
-			time.sleep(0.5)
-			WebDriverWait(self.browser, 20).until(
-				EC.visibility_of_element_located(
-					(By.XPATH, '//div[@class="feed-toggle"]/ul/li/a[@href="#/tag/loret"]')))
-			article_titles = self.browser.find_elements_by_xpath(
-				'//div[@class="article-preview"]//a[@class="preview-link" and contains(@href, "articles")]/h1')
-		except:
-			assert False
-		else:
-			assert True
+
+		loret_tag = self.browser.find_element_by_xpath('//div[@class="sidebar"]/div/a[@href="#/tag/loret"]')
+		loret_tag.click()
+		time.sleep(0.5)
+		WebDriverWait(self.browser, 20).until(
+			EC.visibility_of_element_located(
+				(By.XPATH, '//div[@class="feed-toggle"]/ul/li/a[@href="#/tag/loret"]')))
+		article_titles = self.browser.find_elements_by_xpath(
+			'//div[@class="article-preview"]//a[@class="preview-link" and contains(@href, "articles")]/h1')
+
 		articles = []
 		for title in article_titles:
 			articles.append(title)
@@ -80,16 +73,12 @@ class TestConduit(object):
 
 	def test_new_article(self):
 		#  Új adat bevitel. Létrehozok egy új article-t.
-		try:
-			new_article_nav = self.browser.find_element_by_xpath('//a[@href="#/editor"]')
-			new_article_nav.click()
+		new_article_nav = self.browser.find_element_by_xpath('//a[@href="#/editor"]')
+		new_article_nav.click()
 
-			editor_article(self.browser, new_article['title'], new_article['about'], new_article['descr'],
-						   new_article['tags'])
-		except:
-			assert False
-		else:
-			assert True
+		editor_article(self.browser, new_article['title'], new_article['about'], new_article['descr'],
+					   new_article['tags'])
+
 		# Ellenőrzöm, hogy a létrehozott article felületére irányít az oldal
 		WebDriverWait(self.browser, 20).until(
 			EC.url_matches(f'http://localhost:1667/#/articles/{new_article["about"]}'))
@@ -98,27 +87,20 @@ class TestConduit(object):
 	def test_edit_article(self):
 		#  Meglévő adat módosítás
 		# article módosításához első lépésben megkeresem az article-t, amit módosítani fogok
-		try:
-			my_articles(self.browser)
-			article_to_edit = WebDriverWait(self.browser, 20).until(
-				EC.presence_of_element_located(
-					(By.XPATH, f'//a[@class="preview-link" and @href="#/articles/{new_article["about"]}"]')))
-			article_to_edit.click()
-			time.sleep(1)
-			edit_article_btn = WebDriverWait(self.browser, 20).until(
-				EC.visibility_of_element_located((By.XPATH, f'//a[@href="#/editor/{new_article["about"]}"]')))
-			edit_article_btn.click()
-			time.sleep(0.5)
-			# Módosítom a létrehozott article adatait
-			editor_article(self.browser, modified_article['title'], modified_article['about'],
-						   modified_article['descr'],
-						   modified_article['tags'])
-		except:
-			assert False
-		else:
-			assert True
-		# Megvizsgálom, hogy megjelenik az article oldalán a módosított title
+		my_articles(self.browser)
+		article_to_edit = WebDriverWait(self.browser, 20).until(EC.presence_of_element_located(
+			(By.XPATH, f'//a[@class="preview-link" and @href="#/articles/{new_article["about"]}"]')))
+		article_to_edit.click()
 		time.sleep(1)
+		edit_article_btn = WebDriverWait(self.browser, 20).until(
+			EC.visibility_of_element_located((By.XPATH, f'//a[@href="#/editor/{new_article["about"]}"]')))
+		edit_article_btn.click()
+		time.sleep(0.5)
+		# Módosítom a létrehozott article adatait
+		editor_article(self.browser, modified_article['title'], modified_article['about'], modified_article['descr'],
+					   modified_article['tags'])
+		time.sleep(1)
+		# Megvizsgálom, hogy megjelenik az article oldalán a módosított title
 		modified_article_title = WebDriverWait(self.browser, 20).until(
 			EC.visibility_of_element_located((By.XPATH, '//h1')))
 		assert modified_article_title.text == modified_article['title']
@@ -129,23 +111,19 @@ class TestConduit(object):
 	def test_delete_article(self):
 		# Adat vagy adatok törlése
 		# article törléséhez első lépésben létrehozok egyet, amit majd törölni fogok
-		try:
-			self.browser.find_element_by_xpath('//a[@href="#/editor"]').click()
-			editor_article(self.browser, new_article['title'], new_article['about'], new_article['descr'],
-						   new_article['tags'])
-			time.sleep(0.5)
-			# Törlöm az article-t
-			del_article_btn = WebDriverWait(self.browser, 20).until(
-				EC.visibility_of_element_located((By.XPATH, '//button[@class="btn btn-outline-danger btn-sm"]')))
-			del_article_btn.click()
-			time.sleep(2)
-		except:
-			assert False
-		else:
-			assert True
+		self.browser.find_element_by_xpath('//a[@href="#/editor"]').click()
+		editor_article(self.browser, new_article['title'], new_article['about'], new_article['descr'],
+					   new_article['tags'])
+		time.sleep(0.5)
+		# Törlöm az article-t
+		del_article_btn = WebDriverWait(self.browser, 20).until(
+			EC.visibility_of_element_located((By.XPATH, '//button[@class="btn btn-outline-danger btn-sm"]')))
+		del_article_btn.click()
+		time.sleep(2)
+
 		# Ellenőrzöm, hogy törlés után automatikusan a főoldalra irányít az oldal
 		assert self.browser.current_url == main_page
-		# Ellenőrzöm, hogy a My Articles listájában nem szerepel a kitörölt article, így 1 db article-nek kell lennie
+		# Ellenőrzöm, hogy a My Articles listájában nem szerepel a kitörölt article, így 1 db article-nek kell lennie a listában
 		my_articles(self.browser)
 		my_articles_elements = WebDriverWait(self.browser, 20).until(
 			EC.visibility_of_all_elements_located((By.XPATH, '//a[@class="preview-link"]')))
@@ -154,41 +132,33 @@ class TestConduit(object):
 	def test_datas_to_file(self):
 		# Adatok lementése felületről.
 		# Elmentem a Global Feedben található article-k title-jeit, about-jait és szerzőjét egy .csv fájlba
-		try:
-			article_author = self.browser.find_elements_by_xpath('//a[@class="author"]')
-			article_title = self.browser.find_elements_by_xpath('//a[@class="preview-link"]/h1')
-			article_about = self.browser.find_elements_by_xpath('//a[@class="preview-link"]/p')
 
-			with open('test/general_feed_articles.csv', 'w', newline='') as csvfile:
-				gen_feed_art = csv.writer(csvfile, delimiter=';')
-				gen_feed_art.writerow(['Nr.', 'Title', 'About', 'Author'])
-				for index, title in enumerate(article_title, start=1):
-					gen_feed_art.writerow(
-						[index, title.text, article_about[index - 1].text, article_author[index - 1].text])
-		except:
-			assert False
-		else:
-			assert True
+		article_author = self.browser.find_elements_by_xpath('//a[@class="author"]')
+		article_title = self.browser.find_elements_by_xpath('//a[@class="preview-link"]/h1')
+		article_about = self.browser.find_elements_by_xpath('//a[@class="preview-link"]/p')
+
+		with open('test/general_feed_articles.csv', 'w', newline='') as csvfile:
+			gen_feed_art = csv.writer(csvfile, delimiter=';')
+			gen_feed_art.writerow(['Nr.', 'Title', 'About', 'Author'])
+			for index, title in enumerate(article_title, start=1):
+				gen_feed_art.writerow(
+					[index, title.text, article_about[index - 1].text, article_author[index - 1].text])
+
 		# Ellenőrzöm, hogy az oldalról leszedett lista hossza megegyezik az elmentett csv fájl sorainak számával
 		gf_csv = pd.read_csv('test/general_feed_articles.csv')
 		assert len(article_title) == len(gf_csv.index)
 
 	def test_datas_from_file(self):
-		# Ismételt és sorozatos adatbevitel adatforrásból
-		try:
-			with open('test/test_datas_articles.csv', 'r', encoding='UTF-8') as data_file:
-				data_table = csv.reader(data_file, delimiter=';')
-				next(data_table)
-				i = 0  # Az assert miatt beteszek egy sorszámlálót
-				for row in data_table:
-					i = i + 1
-					new_article_nav = self.browser.find_element_by_xpath('//a[@href="#/editor"]')
-					new_article_nav.click()
-					editor_article(self.browser, row[0], row[1], row[2], row[3])
-		except:
-			assert False
-		else:
-			assert True
+		# Ismételt és sorozatos adatbevitel adatforrásból új article-ek létrehozásához.
+		with open('test/test_datas_articles.csv', 'r', encoding='UTF-8') as data_file:
+			data_table = csv.reader(data_file, delimiter=';')
+			next(data_table)
+			i = 0  # Az assert miatt beteszek egy sorszámlálót
+			for row in data_table:
+				i = i + 1
+				new_article_nav = self.browser.find_element_by_xpath('//a[@href="#/editor"]')
+				new_article_nav.click()
+				editor_article(self.browser, row[0], row[1], row[2], row[3])
 
 		td_csv = pd.read_csv('test/test_datas_articles.csv')
 		assert i == len(td_csv.index)
